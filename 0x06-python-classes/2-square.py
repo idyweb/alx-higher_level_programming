ada267850433135f1b1bf97bc1830b93f102bc1c
idyweb <inyangidongesit22@gmail.com>
2024-01-17 15:20:12 +0100

    2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """Define a class square"""
+
 
 class Square:
     """The class being defined"""
@@ -13,5 +15,5 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0"
+            raise ValueError("size must be >= 0")
         self.__size = size
