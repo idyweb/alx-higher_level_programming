3aa807c0c84e6db3e3dd51a2e779fbff9d645ca0
idyweb <inyangidongesit22@gmail.com>
2024-01-17 15:31:54 +0100

    3square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,8 +17,9 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-    """
-        method that returns the square of the area
-    """
-        return self.__size * self.__size
+        """
+            method that returns the square of the area
+        """
+        return (self.__size * self.__size)
