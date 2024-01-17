38b5ce39cd49405efb4ff75606130baac5841d5c
idyweb <inyangidongesit22@gmail.com>
2024-01-17 15:34:24 +0100

    3square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,11 +7,11 @@ class Square:
     """The class being defined"""
 
     def __init__(self, size=0):
-    """Initializing the class
+        """Initializing the class
 
         Args:
             size: The size of the square
-    """
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
