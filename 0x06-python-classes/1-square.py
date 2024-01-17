79299e67a955949df80879eecd3fe7f209b18792
idyweb <inyangidongesit22@gmail.com>
2024-01-17 15:01:58 +0100

    square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,10 @@
 
 class Square:
     """This is the class that is defined"""
-    __size = None
-    pass
+    def __init__(self, size):
+        """Initialize a new square
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
