461d165acb5a93b82d7a0cc4a66bd677084c1bf1
idyweb <inyangidongesit22@gmail.com>
2024-01-18 09:28:22 +0100

    2-rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,6 +22,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """set the width of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -35,6 +36,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Set height of rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")                                                        if value < 0:
             raise ValueError("height must be >= 0")
