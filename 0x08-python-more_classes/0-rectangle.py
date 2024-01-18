cb34c4fb63f036df0b0f747fd7eff3042c672132
idyweb <inyangidongesit22@gmail.com>
2024-01-18 08:08:24 +0100

    empty rec

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Define an empty class Rectangle"""
+
+
+class Rectangle:
+    """An empty rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+this is the readme for the python more classes
